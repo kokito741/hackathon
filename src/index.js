8c145f3c91339daa4f0f1a4be8086153d57a8c27
Konstantin Kapanov <kokito741@gmail.com>
2025-11-21 14:56:05 +0200

    fixed Nov 21 12:53:48  Error: Cannot find module './src/app'

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 require('dotenv').config();
-const app = require('./src/app');
+const app = require('./app');
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> console.log("Server on "+PORT));
