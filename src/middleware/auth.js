b290c5dd4c36674d4c9ec70af345321b981a34bd
Konstantin Kapanov <kokito741@gmail.com>
2025-11-22 05:45:36 +0200

    added debuging for token

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,8 +4,9 @@ module.exports=(req,res,next)=>{
   if(!h) return res.status(401).json({error:'missing token'});
   try{
     req.user=jwt.verify(h.split(' ')[1], process.env.JWT_SECRET);
+    console.log('Middleware JWT_SECRET:', process.env.JWT_SECRET);
+    console.log('Token received:', h.split(' ')[1]);
     next();
   }catch(e){ res.status(401).json({error:'invalid token'}); 
-  res.redirect('/');
 }
 };
